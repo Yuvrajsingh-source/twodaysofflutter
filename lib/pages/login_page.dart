371f532739d7df1848698d1bbeaaadf0fcf2cab6
Yuvrajsingh-source <yuvraajsingh9301@gmail.com>
2021-05-19 19:48:48 +0530

    day5completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,16 @@
+import 'package:catalog/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-class Loginpage extends StatelessWidget {
+class Loginpage extends StatefulWidget {
+  Loginpage({Key key}) : super(key: key);
+
+  @override
+  _LoginpageState createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  String name = "";
+  bool changedButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -14,11 +24,15 @@ class Loginpage extends StatelessWidget {
               children: [
                 Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
                 SizedBox(height: 20),
-                Text("Welcome",
+                Text("Welcome $name",
                     style:
                         TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
                 SizedBox(height: 20),
                 TextFormField(
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                     decoration: InputDecoration(
                         hintText: "Enter Username", labelText: "Username")),
                 TextFormField(
@@ -28,9 +42,36 @@ class Loginpage extends StatelessWidget {
                 SizedBox(
                   height: 20.0,
                 ),
-                ElevatedButton(
-                  onPressed: null,
-                  child: Text("Login"),
+                InkWell(
+                  onTap: () async {
+                    changedButton = true;
+                    setState(() {});
+                    await Future.delayed(Duration(seconds: 2));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Homepage()));
+                  },
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(changedButton ? 50 : 8)),
+                    width: changedButton ? 50 : 150,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: changedButton
+                        ? Icon(
+                            Icons.done,
+                            color: Colors.white,
+                          )
+                        : Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                  ),
                 )
               ],
             ),
