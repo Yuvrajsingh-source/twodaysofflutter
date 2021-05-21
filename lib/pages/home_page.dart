c562ac44fc4f7f4e383f2b0757456a7aea3ac6a8
Yuvrajsingh-source <yuvraajsingh9301@gmail.com>
2021-05-21 17:50:53 +0530

    day9completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,18 +34,18 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(left: 80),
-          child: Text(
-            'Catalog App',
-            style: TextStyle(color: Colors.black, fontSize: 19),
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(left: 80),
+            child: Text(
+              'Catalog App',
+              style: TextStyle(color: Colors.black, fontSize: 19),
+            ),
           ),
         ),
-      ),
-      drawer: Mydrawer(),
-      body: Padding(
-          padding: const EdgeInsets.all(0),
+        drawer: Mydrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(0.0),
           child: (catalogModels.items != null && catalogModels.items.isNotEmpty)
               ? ListView.builder(
                   itemCount: catalogModels.items.length,
@@ -57,7 +57,7 @@ class _HomepageState extends State<Homepage> {
                 )
               : Center(
                   child: CircularProgressIndicator(),
-                )),
-    );
+                ),
+        ));
   }
 }
--- lib/widget/item_widget.dart
+++ lib/widget/item_widget.dart
@@ -12,16 +12,22 @@ class ItemWidgets extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
         child: ListTile(
           onTap: () {
             print(item.name);
           },
-          leading: Image.network(item.image),
+          leading: Image.network(
+            item.image,
+            height: 40,
+          ),
           title: Text(item.name),
           subtitle: Text(item.desc),
           trailing: Text(
             "\$${item.price.toString()}",
-            textScaleFactor: 1.2,
+            textScaleFactor: 1.0,
             style: TextStyle(
               color: Colors.deepPurple,
               fontWeight: FontWeight.bold,
